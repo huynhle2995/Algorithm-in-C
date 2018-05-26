998d76982608ba1ccbe3fbe222c8d26bf7292dd3
huynhle2995 <39648282+huynhle2995@users.noreply.github.com>
2018-05-26 18:58:46 +0700

    Update and rename 1_sieve-of-Eratosthenes to 1_sieve-of-Eratosthenes.c

---
+++ 1_sieve-of-Eratosthenes.c
@@ -0,0 +1,14 @@
+/* These algorithm that created by Eratosthenes */
+/* count primes that smaller 100 */
+
+#include <stdio.h>
+
+#define N 1000
+
+int main()
+{
+	int i,j a[N=1];
+	
+	for (a[1] = 0, i=2; i<=N; i++)	// the loop to 
+		a[i] = 1;
+}
