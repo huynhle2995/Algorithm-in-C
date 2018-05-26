b6a6159a29bdc89e1da688e3af08efa542ea41d3
huynhle2995 <39648282+huynhle2995@users.noreply.github.com>
2018-05-26 19:46:13 +0700

    Update 1_sieve-of-Eratosthenes.c

--- 1_sieve-of-Eratosthenes.c
+++ 1_sieve-of-Eratosthenes.c
@@ -7,8 +7,13 @@
 
 int main()
 {
-	int i,j a[N=1];
+	int i,j a[N+1];
 	
-	for (a[1] = 0, i=2; i<=N; i++)	// the loop to 
-		a[i] = 1;
+	for (a[1] = 0, i=2; i<=N; i++)	// the loop to store value of a[i] , with i belong [2, 1000].
+		a[i] = 1;		// a[i] = 1, with a[2]=1, a[3]=1, a[4]=1 ...
+					/// progress to store 999 values by 1
+	
+	for (i=2; i <= N/2; i++)	// i belong [2, 500].
+		for (j=2; j <= N/i; j++)
+			a[i*j] = 0;
 }
